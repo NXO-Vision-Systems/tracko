488c6a769ce9875ac3fc6bb8cb2c8e83b5db2d89
NXO-Vision-Systems <enochthegoat23@gmail.com>
2026-06-13 15:53:57 -0400

    chore: rename app to Tracko

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.noxofy.app',
-  appName: 'Noxofy',
+  appId: 'com.tracko.app',
+  appName: 'Tracko',
   webDir: 'out',
   server: {
     url: 'https://tracko-nine.vercel.app',
